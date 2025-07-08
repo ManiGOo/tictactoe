058d08852c27bc20624649ca417210cab52a06bb
Manish <its.nathmanish@gmail.com>
2025-07-08 14:17:29 +0530

    Code clean up for cli version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { startGame } from './game.js'
-import { makeMove, switchPlayer, checkWinner } from './playerTurn.js'
-import { board, printBoard } from './board.js'
 
 
 startGame();
-printBoard();
